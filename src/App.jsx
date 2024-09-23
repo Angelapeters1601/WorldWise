123c7c9784daf0bc4e7cb25f29526e60d7fdbe83
Angelapeters1601 <nwattaha@gmail.com>
2024-09-23 02:09:16 +0200

    Used navigate for programatic navigation and going back in history , made a reusable button component with type variant for setting styles. used 'Navigate' in index route for redirecting and 'replace' to go back

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill";
 
@@ -12,6 +12,7 @@ import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 polyfillCountryFlagEmojis();
 
@@ -47,7 +48,7 @@ function App() {
         <Route path="/app" element={<AppLayout />}>
           <Route
             index
-            element={<CityList cities={cities} isLoading={isLoading} />}
+            element={<Navigate replace to="cities" isLoading={isLoading} />}
           />
           <Route
             path="cities"
@@ -59,7 +60,7 @@ function App() {
             path="country"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
