e7f91732cf222deade9f0ff677f09b409f5efa65
Angelapeters1601 <nwattaha@gmail.com>
2024-10-01 23:25:11 +0100

    installed leaflet, added and customized map and its components

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,26 +1,44 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useState } from "react";
+// import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useCities } from "../contexts/CitiesContext";
 import styles from "./Map.module.css";
 
 function Map() {
-  const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
+  //   const navigate = useNavigate();
+  const { cities } = useCities();
+  //   const [searchParams, setSearchParams] = useSearchParams();
+  const [mapPosition, setmapPosition] = useState([40, 0]);
 
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  //   const lat = searchParams.get("lat");
+  //   const lng = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <h1>
-        Position: {lat}, {lng}
-      </h1>
-      <button
-        onClick={() => {
-          setSearchParams({ lng: 30, lat: 50 });
-        }}
+    <div
+      className={styles.mapContainer}
+      //  onClick={() => navigate("form")}
+    >
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
       >
-        Change Position
-      </button>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
